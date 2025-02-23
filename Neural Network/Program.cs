a6f46789a2581fbf34798efca16af661d22a0199
Renan Lopes <renan-lopes123@hotmail.com>
2025-02-23 12:29:10 -0300

    Add Project

---
+++ Neural Network/Program.cs
@@ -0,0 +1,22 @@
+﻿var neuralNetwork = CreateNeuralNetwork();
+
+
+NeuralNetwork CreateNeuralNetwork()
+{
+    return new NeuralNetwork();
+}
+
+public class NeuralNetwork
+{
+    public List<Layer> Layers { get; private set; }
+    public double Prediction { get; private set; }
+    public ILossFuntion Loss { get; private set; }
+}
+
+public class Layer
+{
+}
+
+public interface ILossFuntion
+{
+}
